2fe8b636f37e4ffb8e4a31d242fd795f7bdd0ca2
yagishuhei <shuhei.y3166@gmail.com>
2022-07-22 14:55:16 +0000

    [Fix]code,miss

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,4 @@
-class FavoretesController < ApplicationController
+class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "c6e233fdfd12423583a81485ea5a7ee9.vfs.cloud9.us-east-1.amazonaws.com"
   config.active_job.queue_adapter = :inline
+  config.hosts << "3f30d27c7fd145d6aae20c69865d30c4.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
