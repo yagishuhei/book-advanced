7c0246b06e10a3802f450f1ac864934a5bee8729
yagishuhei <shuhei.y3166@gmail.com>
2022-07-25 14:54:39 +0000

    [Fix]favorite,create,destroy

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,6 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
   end
 
 
@@ -11,6 +10,5 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to book_path(book)
   end
 end
