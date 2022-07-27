78d804a78fd1527c1afae0cd46149aa1927775ee
yagishuhei <shuhei.y3166@gmail.com>
2022-07-27 15:14:47 +0000

    [Add]jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
